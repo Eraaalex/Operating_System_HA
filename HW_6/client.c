fa5435072bc3c66be600f6639825e9ca116babb6
Eralex <eralex135@gmail.com>
2024-03-04 12:23:04 +0300

    HW6 2

--- HW_6/client.c
+++ HW_6/client.c
@@ -1,58 +1,48 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <sys/shm.h>
-#include <sys/stat.h>
-#include <time.h>
 #include <unistd.h>
+#include <sys/shm.h>
+#include <stdlib.h>
+#include <time.h>
+#include <error.h>
+#include <signal.h>
 
-#define SHM_KEY 1234
-typedef struct {
-    int data;
-    volatile int flag; // 0 - данные не готовы, 1 - данные готовы для чтения
-} shared_data;
+int shm_id;
+int *share;
+int server_pid;
 
+void cleanup(int signum) {
+    if (signum == SIGINT) {
+        kill(server_pid, SIGINT);
+    }
+    shmdt(share);
+    shmctl(shm_id, IPC_RMID, NULL);
+    exit(0);
+}
 
 int main() {
-    int shmid;
-    shared_data *data;
-
-    // Получаем доступ к сегменту разделяемой памяти
-    shmid = shmget(SHM_KEY, sizeof(shared_data), 0666);
-    if (shmid < 0) {
-        perror("shmget");
-        exit(1);
-    }
-
-    // Прикрепляем сегмент разделяемой памяти
-    data = (shared_data *)shmat(shmid, NULL, 0);
-    if (data == (shared_data *)(-1)) {
-        perror("shmat");
-        exit(1);
-    }
-
-    // Инициализируем генератор случайных чисел
+    printf("[Client] PID: %d\n", getpid());
+    printf("Please, enter the pid of the server: ");
+    scanf("%d", &server_pid);
+    int num = 0;
+    signal(SIGINT, cleanup);
     srand(time(NULL));
-
-    for (int i = 0; i < 10; ++i) { // Ограничим количество чисел для примера
-        while (data->flag == 1) {
-            sleep(1); // Ожидаем, пока сервер не прочитает предыдущее число
-        }
-
-        // Генерация случайного числа и запись в разделяемую память
-        data->data = rand() % 100; // Случайное число в пределах от 0 до 99
-        data->flag = 1; // Устанавливаем флаг готовности данных
-        printf("Sent: %d\n", data->data);
+    shm_id = shmget(0x2FF, getpagesize(), 0666 | IPC_CREAT);
+    printf("Shared Memory Segment id is %d\n", shm_id);
+    if (shm_id < 0) {
+        perror("Error 1");
+        exit(1);
+    }
+    share = (int *) shmat(shm_id, 0, 0);
+    if (share == NULL) {
+        perror("Error 2");
+        exit(2);
     }
 
-    // Сигнализируем серверу о завершении работы
-    while (data->flag == 1) {
-        sleep(1); // Ожидаем, пока сервер не прочитает последнее число
+    while (1) {
+        num = random() % 1000;
+        *share = num;
+        printf("[Client] sent: %d\n", num);
+        sleep(1);
     }
-    data->data = -1; // Специальное значение для завершения сервера
-    data->flag = 1;
-
-    // Отсоединяемся от сегмента разделяемой памяти
-    shmdt((void *) data);
-
     return 0;
 }
